bbe29b8396defe034026d13fe9dedb679431ed41
Gennady Krasnikov <gennady.krasnikov@gmail.com>
2017-03-15 20:03:47 +0300

    Задание 1. Подготовьте инфраструктуру

---
+++ selenium/python-example/test_webrtc_edge.py
@@ -0,0 +1,22 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Edge()
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(driver):
+    # Todo: microphone sharing notification
+    # Todo: better check points, because now Test passes successful but feature doesn't work(!!!)
+    driver.get("https://webrtc.github.io/samples/src/content/peerconnection/audio/")
+    driver.find_element_by_id('callButton').click()
+    time.sleep(5)
+    driver.find_element_by_id('hangupButton').click()
+    time.sleep(5)
---
+++ selenium/python-example/test_webrtc_opera.py
@@ -0,0 +1,23 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Opera()
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(driver):
+    # Doesn't work at all for Opera 43, opera opens and closes in a moment
+    # driver https://github.com/operasoftware/operachromiumdriver/releases/tag/v0.2.2
+    driver.get("https://webrtc.github.io/samples/src/content/peerconnection/audio/")
+    time.sleep(5)
+    driver.find_element_by_id('callButton').click()
+    time.sleep(5)
+    driver.find_element_by_id('hangupButton').click()
+    time.sleep(5)
