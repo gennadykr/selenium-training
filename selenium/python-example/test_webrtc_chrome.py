f39523f64d003fb87ebc2771e89bfc805c6eec01
Gennady Krasnikov <gennady.krasnikov@gmail.com>
2017-03-25 06:57:13 +0300

    3. Запуск браузеров, установка разрешений на микрофон для Chrome и Firefox
    Chrome: надо использовать параметр командной строки
    --use-fake-ui-for-media-stream
    
    FF: надо подключить при запуске профайл с permissions.sqlite, где
    сохранено разрешения для сайта использовать микрофон

--- selenium/python-example/test_webrtc_chrome.py
+++ selenium/python-example/test_webrtc_chrome.py
@@ -7,17 +7,25 @@ from selenium.webdriver.support import expected_conditions as EC
 
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Chrome()
+    # Using of command line switch to enable microphone
+    # Note that Crome has many helpfull command line switches for WebRTC testing!
+
+    # Variant 1:
+    # wd = webdriver.Chrome(desired_capabilities={"chromeOptions":{"args":["--use-fake-ui-for-media-stream"]}})
+
+    # Variant 2:
+    options = webdriver.ChromeOptions()
+    #options.binary_location = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+    options.add_argument("use-fake-ui-for-media-stream")
+    wd = webdriver.Chrome(chrome_options=options)
     print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
 
-
 def test_example(driver):
-    # Todo: microphone sharing notification
-    # Todo: better checs, test passes successfull now while feature doesn't work (!!!)
+    # Todo: better chekcs
     driver.get("https://webrtc.github.io/samples/src/content/peerconnection/audio/")
     driver.find_element_by_id('callButton').click()
-    time.sleep(5)
+    time.sleep(10)
     driver.find_element_by_id('hangupButton').click()
     time.sleep(5)
--- selenium/python-example/test_webrtc_firefox.py
+++ selenium/python-example/test_webrtc_firefox.py
@@ -7,7 +7,17 @@ from selenium.webdriver.support import expected_conditions as EC
 
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Firefox(firefox_binary='c:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe')
+    # Set profile with microfone permission (unpack it from 4uxxq5yw.microphone.7z):
+    profile = webdriver.FirefoxProfile()
+    profile.profile_dir = "C:/GitHub/selenium-training/selenium/python-example/4uxxq5yw.microphone"
+    # BUG: works only with /, but not with \\
+    # BUG: FF remove original profile after exit!!! Unpack it before test in temporary folder?
+    # TODO: how to minimize profile? Only permissions.sqlite is relevant
+
+    # exact place for 32bit version only:
+    # wd = webdriver.Firefox(firefox_binary='c:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe')
+
+    wd = webdriver.Firefox(profile)
     print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
@@ -17,6 +27,7 @@ def test_example(driver):
     # Todo: microphone sharing notification
     # hangupButton appears to be disabled (OK) - test fails
     driver.get("https://webrtc.github.io/samples/src/content/peerconnection/audio/")
+    time.sleep(5)
     driver.find_element_by_id('callButton').click()
     time.sleep(5)
     driver.find_element_by_id('hangupButton').click()
