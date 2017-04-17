cd96b4ecc45dba2fd649a17adc755bab7729f99e
Gennady Krasnikov <gennady.krasnikov@gmail.com>
2017-04-17 11:04:20 +0300

    Задание 12. Сделайте сценарий добавления товара

--- selenium/python-example/test_litecart_admin_add_product.py
+++ selenium/python-example/test_litecart_admin_add_product.py
@@ -20,6 +20,15 @@ def driver(request):
     request.addfinalizer(wd.quit)
     return wd
 
+def select_element_by_text(elements, pattern):
+    assert len(elements)>0, "Empty list of elements"
+    for x in range(0, len(elements)):
+        if (pattern in elements[x].text):
+            elements[x].click()
+            return elements[x]
+    raise("No element found with "+pattern)
+
+
 def test_example(driver):
     # Log in to admin
     driver.get("http://localhost/litecart/admin/")
@@ -30,22 +39,20 @@ def test_example(driver):
     # Open Catalog menu
     wait = WebDriverWait(driver, 15)
     menu_items = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR , "#app- > a")))
-    for x in range(0, len(menu_items)):
-         if ('Catalog' in menu_items[x].text):
-            menu_items[x].click()
-            break
+    select_element_by_text(menu_items,'Catalog')
+    #wait.until(EC.presence_of_all_elements_located((By.XPATH, "//a/*[contains(.,'Catalog')]")))[0].click()
 
     # Click on "Add new Product" button
     add_buttons = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR , "#content a.button")))
-    for x in range(0, len(add_buttons)):
-        if ('Product' in add_buttons[x].text):
-            add_buttons[x].click()
-            break
+    select_element_by_text(add_buttons,'Product')
+    #wait.until(EC.presence_of_all_elements_located((By.XPATH, "//a[contains(.,'Add New Product')]")))[0].click()
 
     # Fill the form
     product_name = 'Chizhik-Pyzhik'
     product_code = 'ch001'
-    product_image = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),'chizhik_pyzhik.jpg'))
+    product_image = \
+        os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),'chizhik_pyzhik_150x150.png'))
+    print(product_image)
 
     tab_general = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR , "#tab-general")))[0]
     tab_general.find_element(By.CSS_SELECTOR,"input[type=radio]").click()
@@ -60,5 +67,32 @@ def test_example(driver):
     selector.select_by_visible_text('Temporary sold out')
 
     tab_general.find_element(By.CSS_SELECTOR,"input[name='new_images[]'").send_keys(product_image)
+
+    # Switch on Information tab
+    #select_element_by_text(driver.find_elements(By.CSS_SELECTOR,".tabs .index a"),'Information')
+    driver.find_element(By.XPATH,"//a[contains(.,'Information')]").click()
+    tab_information = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR , "#tab-information")))[0]
+    manufacturer_selector = tab_information.find_element(By.CSS_SELECTOR,"select[name=manufacturer_id]")
+    selector = Select(manufacturer_selector)
+    selector.select_by_visible_text('ACME Corp.')
+    tab_information.find_element(By.CSS_SELECTOR,"[name='short_description[en]']").send_keys(product_name)
+    tab_information.find_element(By.CSS_SELECTOR,".trumbowyg-editor").send_keys('Crystal crafts')
+
+    # Switch on Information tab
+    driver.find_element(By.XPATH,"//a[contains(.,'Prices')]").click()
+    tab_prices = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR , "#tab-prices")))[0]
+    price_field=tab_prices.find_element(By.CSS_SELECTOR,"input[name=purchase_price]")
+    price_field.clear()
+    price_field.send_keys("3")
+    tab_prices.find_element(By.CSS_SELECTOR,"input[name='prices[USD]']").send_keys("6")
+
+    # Save
+    driver.find_element(By.CSS_SELECTOR,"button[name=save]").click()
+
+    # Check
+    xpath_string = './/a[contains(.,\''+product_name+'\')]'
+    print(xpath_string)
+    catalog_form = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR , "form[name=catalog_form")))[0]
+    catalog_form.find_element(By.XPATH,xpath_string).click()
 
     time.sleep(5)
