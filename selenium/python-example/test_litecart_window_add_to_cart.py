67b2c38e8f64b401f15a6a3a5ad2bf426c0d1491
Gennady Krasnikov <gennady.krasnikov@gmail.com>
2017-04-17 23:09:07 +0300

    WIP: Задание 13. Сделайте сценарий работы с корзиной

--- selenium/python-example/test_litecart_window_add_to_cart.py
+++ selenium/python-example/test_litecart_window_add_to_cart.py
@@ -29,9 +29,11 @@ def test_cart(driver):
 
         # Add the product to the cart
         box_product = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR , "#box-product")))[0]
+
         cart_item_quantity_element = driver.find_element(By.CSS_SELECTOR,"#cart-wrapper .quantity")
         cart_item_quantity = int(cart_item_quantity_element.text)
         print(cart_item_quantity)
+
         selectors = box_product.find_elements(By.CSS_SELECTOR , "select[name='options[Size]']")
         if (len(selectors) > 0):
             Select(selectors[0]).select_by_index(1)
@@ -39,7 +41,28 @@ def test_cart(driver):
 
         # Waiting new quantity counter in the cart
         while int(cart_item_quantity_element.text) <= cart_item_quantity:
-            time.sleep(1)
+            time.sleep(0.5)
+
+        # How to write lambda for found element, without selector?
+        #wait.until(lambda cart_item_quantity_element: int(cart_item_quantity_element.text)>cart_item_quantity) - fails
+        #wait.until(EC.visibility_of(element)) - works
+
+        # With selector:
+        #wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR,"#cart-wrapper .quantity"),str(cart_item_quantity+1)))
         print(int(cart_item_quantity_element.text))
 
-    time.sleep(10)
+
+    # Go to the cart
+    driver.find_element(By.XPATH,"//*[@id='cart-wrapper']//a[.//*[contains(@class,'quantity')]]").click()
+    #cart_item_quantity_element.find_element(By.XPATH,".//..").click()
+
+    # Remove products:
+    products_shortcuts = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR , ".shortcuts a")))
+    while len(products_shortcuts) > 0:
+        products_shortcuts[0].click()
+        first_table_line = driver.find_elements(By.CSS_SELECTOR,".dataTable.rounded-corners tr:not(.header)")[0]
+        wait.until(EC.visibility_of(driver.find_elements(By.CSS_SELECTOR , "button[name=remove_cart_item]")[0])).click()
+        wait.until(EC.staleness_of(first_table_line))
+        products_shortcuts = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR , ".shortcuts a")))
+
+    time.sleep(3)
